5232569f4912f05a56a8e4e81ba09e7f6eb48ca3
Jan Haans <jan.haans@gmail.com>
2024-09-05 12:49:36 +0200

    Attach IAM policy to IAM user

--- create-iam-user/main.tf
+++ create-iam-user/main.tf
@@ -1,8 +1,11 @@
-resource "aws_iam_user" "example" {
+resource "aws_iam_user" "user" {
     name = var.user_name
     path = "/"
 
-    tags = {
-        Name        = "Anna-Maria"
-    }
+    tags = var.user_name_tags
+}
+
+resource "aws_iam_user_policy_attachment" "test-attach" {
+  user       = aws_iam_user.user.name
+  policy_arn = var.policy_arn
 }
--- create-iam-user/variables.tf
+++ create-iam-user/variables.tf
@@ -2,3 +2,15 @@ variable "user_name" {
   type = string
   default = "kees"
 }
+
+variable "user_name_tags" {
+  type = map(string)
+  default = {
+    Name = "kees"
+  }
+}
+
+variable "policy_arn" {
+  type = string
+  default = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
